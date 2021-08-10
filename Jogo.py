c2f1c1c50d59a6bee5cd8494ae20f1474595debb
andreimp2021 <andreimp@al.insper.edu.br>
2021-08-10 20:42:21 -0300

    Desenhei um texto na tela

--- Jogo.py
+++ Jogo.py
@@ -6,8 +6,8 @@ pygame.init()
 
 # ----- Gera tela principal
 
-WIDTH = 1300 # Largura
-HEIGHT = 650 # Altura
+WIDTH = 500 # Largura
+HEIGHT = 400 # Altura
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Jogo da Nave')
@@ -15,16 +15,21 @@ pygame.display.set_caption('Jogo da Nave')
 # ----- Inicia estruturas de dados
 game = True
 
+# ----- Inicia assets
+font = pygame.font.SysFont(None, 48)
+text = font.render('HELLO WORLD', True, (0, 0, 255))
+
 # ===== Loop principal =====
 while game:
     # ----- Trata eventos
     for event in pygame.event.get():
         # ----- Verifica consequências
-        if event.type == pygame.QUIT or event.type == pygame.KEYUP :
+        if event.type == pygame.QUIT:
             game = False
 
     # ----- Gera saídas
     window.fill((255, 255, 255))  # Preenche com a cor branca
+    window.blit(text, (10, 10))
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
