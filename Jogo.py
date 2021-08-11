d15c09184295562bf7d59b9f7b088722aaeb26b1
andreimp2021 <andreimp@al.insper.edu.br>
2021-08-10 21:26:54 -0300

    adicionei um meteoro ao jogo

--- Jogo.py
+++ Jogo.py
@@ -12,12 +12,21 @@ HEIGHT = 400 # Altura
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Jogo da Nave')
 
+# ----- Inicia assets
+METEOR_WIDTH = 50
+METEOR_HEIGHT = 38
+font = pygame.font.SysFont(None, 48)
+background = pygame.image.load('assets/img/starfield.png').convert()
+meteor_img = pygame.image.load('assets/img/meteorBrown_med1.png').convert_alpha()
+meteor_img_small = pygame.transform.scale(meteor_img, (METEOR_WIDTH, METEOR_HEIGHT))
+
 # ----- Inicia estruturas de dados
 game = True
-
-# ----- Inicia assets
-font = pygame.font.SysFont(None, 48)
-text = font.render('HELLO WORLD', True, (0, 0, 255))
+meteor_x = 200
+# y negativo significa que está acima do topo da janela. O meteoro começa fora da janela
+meteor_y = -METEOR_HEIGHT
+meteor_speedx = 3
+meteor_speedy = 4
 
 # ===== Loop principal =====
 while game:
@@ -27,11 +36,19 @@ while game:
         if event.type == pygame.QUIT:
             game = False
 
-    # ----- Gera saídas
-    window.fill((255, 255, 255))  # Preenche com a cor branca
-    window.blit(text, (10, 10))
-
     # ----- Atualiza estado do jogo
+    # Atualizando a posição do meteoro
+    meteor_x += meteor_speedx
+    meteor_y += meteor_speedy
+    # Se o meteoro passar do final da tela, volta para cima
+    if meteor_y > HEIGHT or meteor_x + METEOR_WIDTH < 0 or meteor_x > WIDTH:
+        meteor_x = 200
+        meteor_y = -METEOR_HEIGHT
+
+    # ----- Gera saídas
+    window.fill((0, 0, 0))  # Preenche com a cor branca
+    window.blit(background, (0, 0))
+    window.blit(meteor_img_small, (meteor_x, meteor_y))
     pygame.display.update()  # Mostra o novo frame para o jogador
 
 # ===== Finalização =====
